a778ae8f65dd5cb7d436824bd7c7801c0fa20362
ICC TEAM <131507368+GOVIND-BOTS@users.noreply.github.com>
2023-05-28 22:48:15 +0530

    config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
-OWNER_ID = int(getenv("OWNER_ID"))
+OWNER_ID = int(getenv("OWNER_ID", "5348648456"))
 
 PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/fd2d1b9bdef248cd900f9.jpg")
 START_IMG = getenv("START_IMG", "https://te.legra.ph/file/e6bd1a3f1fe9a62328b07.jpg")
@@ -21,7 +21,7 @@ SESSION = getenv("SESSION", None)
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/indian_chatting_club_offical")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/tha_govind_op")
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1864894033").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6020570673").split()))
 
 
 FAILED = "https://te.legra.ph/file/9f62f4091405b8476b379.jpg"
